d3c7172a99f793dfeccaca0d8b410ffc2a175bfd
kt999 <kiteak9275@gmail.com>
2020-04-05 20:58:47 +0900

    next 셋팅

---
+++ front/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = () => {
+    return (
+        <>
+            <div>lending page</div>
+            <Link href="/main"><a>main page</a></Link>
+        </>
+    )
+};
+
+export default  Home;
